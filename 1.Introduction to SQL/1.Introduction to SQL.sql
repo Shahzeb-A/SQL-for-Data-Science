705faa8db6cea7767f4db0feb7c7f8d5aeaf3adb
Shahzeb Ali <63748662+Shahzeb-A@users.noreply.github.com>
2023-03-25 16:00:47 +0300

    Add files via upload

---
+++ 1.Introduction to SQL/1.Introduction to SQL.sql
@@ -0,0 +1,20 @@
+# Database is collection of tables
+# Tables is nothing but a tableaur data whhich has rows and columns 
+
+# Number of sheets in excel file is equal to Number of tables in MySQL Work
+
+# That means the tables you see in the moviesdb is like the multiple sheets you see in excel
+# You can view the table by clicking on the desired table and like "movies" and selecting the third icon
+
+# There are total 5 tables:
+# 1.actors
+# 2.financials
+# 3.languages
+# 4.movie_actor
+# 5.movies
+
+# The simplest and recommended method is to download MySQL Installer (for Windows) and let it install and configure a specific version of MySQL Server as follows:
+# Download MySQL Installer from https://dev.mysql.com/downloads/installer/ and execute it.
+
+# TO see the dataframe:
+select * from moviesdb.movies;
